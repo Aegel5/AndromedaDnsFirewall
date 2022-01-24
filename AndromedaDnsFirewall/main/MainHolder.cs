3e1ada67c91b0d75281729ee6250d71dc52a9a1b
alex <alexzhigay@mail.ru>
2022-01-24 21:07:31 +0300

    1

--- AndromedaDnsFirewall/main/MainHolder.cs
+++ AndromedaDnsFirewall/main/MainHolder.cs
@@ -98,11 +98,11 @@ namespace AndromedaDnsFirewall.main
                 reqId = req.Id;
                 bool wasRemoved = false;
 
-                //Question fortest = null;
+                Question fortest = null;
 
                 for (int i = req.Questions.Count - 1; i >= 0; i--) {
                     var quest = req.Questions[i];
-                    //fortest = quest;
+                    fortest = quest;
                     var name = quest.Name.ToString();
                     var dnsElem = new DnsElem(quest.Type, quest.Class, name);
 
@@ -165,8 +165,9 @@ namespace AndromedaDnsFirewall.main
                     dnsItem.answ = lazyres.BuffGet;
                 } else {
                     Message msg = new() { Id = reqId };
-                    msg.Status = MessageStatus.Refused; // nobody check this :(
-                    //msg.Answers.Add(new ARecord() { Name = fortest.Name, Address = IPAddress.Parse("0.0.0.0") });
+                    //msg.Status = MessageStatus.Refused; // nobody check this :(
+                    //msg.Answers.Add(new CNAMERecord() { Name = fortest.Name, Target = fortest.Name });
+                    msg.Answers.Add(new ARecord() { Name = fortest.Name, Address = IPAddress.Parse("127.0.0.1") });
                     dnsItem.answ = msg.ToByteArray();
                 }
 
